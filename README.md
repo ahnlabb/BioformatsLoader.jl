c9de643aac1584049c66973b898ee1ceb4bddafa
Johannes Ahnlide <johannes@voxel.se>
2018-02-05 13:01:46 +0100

    Implemented `bf_import`
    - Created the function `bf_import` that opens a file with an
    OMEXMLReader and returns a list with all images with some metadata
    - Added readme that shows basic usage of `bf_import`

--- README.md
+++ README.md
@@ -5,3 +5,26 @@
 [![Coverage Status](https://coveralls.io/repos/ahnlabb/BioformatsLoader.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/ahnlabb/BioformatsLoader.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/ahnlabb/BioformatsLoader.jl/coverage.svg?branch=master)](http://codecov.io/github/ahnlabb/BioformatsLoader.jl?branch=master)
+
+## Usage
+
+Requires `bioformats_package.jar`
+
+```julia
+Pkg.clone("https://github.com/ahnlabb/BioformatsLoader.jl")
+```
+
+```julia
+using BioformatsLoader
+using JavaCall
+using BioFormats
+using Images
+
+try
+        JavaCall.init(["-ea", "-Xmx1024M", "-Djava.class.path=/path/to/bioformats_package.jar"])
+end
+
+image = bf_import("file.msr")
+```
+
+where `/path/to/bioformats_package.jar` should be changed to fit your setup.
--- src/BioformatsLoader.jl
+++ src/BioformatsLoader.jl
@@ -1,5 +1,108 @@
 module BioformatsLoader
+using JavaCall
+using LightXML
+using ImageMetadata
 
-# package code goes here
+export
+	create_service,
+	set_id,
+	OMEXMLReader,
+	openbytes,
+	set_series,
+	getlength,
+	getpixeltype,
+	open_img,
+	bf_import
 
-end # module
+const JServiceFactory = @jimport loci.common.services.ServiceFactory
+const JOMEXMLService = @jimport loci.formats.services.OMEXMLService #unused
+const JService = @jimport loci.common.services.Service
+const JIFormatReader = @jimport loci.formats.IFormatReader
+const JImageReader = @jimport loci.formats.ImageReader
+const JOMEXMLMetadata = @jimport loci.formats.ome.OMEXMLMetadata
+const JMetadataStore = @jimport loci.formats.meta.MetadataStore
+
+const JLength = @jimport ome.units.quantity.Length
+
+function create_service(classname::String)
+        factory = JServiceFactory(())
+        cls = classforname(classname)
+        service = jcall(factory, "getInstance", JService, (JClass,), cls)
+        return convert(JavaObject{Symbol(classname)}, service)
+end
+
+struct OMEXMLReader
+        reader::JavaObject
+        meta::JavaObject
+end
+
+function OMEXMLReader()
+        service = create_service("loci.formats.services.OMEXMLService")
+        meta = jcall(service, "createOMEXMLMetadata", JOMEXMLMetadata, ())
+        reader = convert(JIFormatReader, JImageReader(()))
+
+        jcall(reader, "setMetadataStore", Void, (JMetadataStore,), meta)
+        OMEXMLReader(reader, meta)
+end
+
+
+function set_id(oxr::OMEXMLReader, fname::AbstractString)
+        jcall(oxr.reader, "setId", Void, (JString,), string(fname))
+end
+
+function openbytes(oxr::OMEXMLReader, index::Int)
+        jcall(oxr.reader, "openBytes", Vector{jbyte}, (jint,), index)
+end
+
+function set_series(oxr::OMEXMLReader, index::Int)
+        jcall(oxr.reader, "setSeries", Void, (jint,), index)
+end
+
+function getlength(oxr::OMEXMLReader, name::String, imageindex::Int)
+        jcall(oxr.meta, "get" * name, JLength, (jint,), imageindex)
+end
+
+function getpixeltype(oxr::OMEXMLReader)
+        id = jcall(oxr.reader, "getPixelType", jint, ())
+	return datatypes[id+1]
+end
+
+function islittleendian(oxr::OMEXMLReader)
+        jcall(oxr.reader, "isLittleEndian", jboolean, ())
+end
+
+function open_img(oxr::OMEXMLReader, index::Int)
+	@assert islittleendian(oxr) == 1
+	sx = jcall(oxr.reader, "getSizeX", jint, ())
+	sy = jcall(oxr.reader, "getSizeY", jint, ())
+	println((sx, sy))
+	arr = reinterpret(getpixeltype(oxr), openbytes(oxr, index))
+	return reshape(arr, (sx, sy))
+end
+
+datatypes = [Int8,UInt8,Int16,UInt16,Int32,UInt32,Float32,Float64,Bool]
+
+function bf_import(fname::String)
+	oxr = OMEXMLReader()
+	set_id(oxr, fname)
+	xml = parse_string(jcall(oxr.meta, "dumpXML", JString, ()))
+	images = Array{ImageMeta,1}()
+
+	metalst = get_elements_by_tagname(root(xml), "Image")
+	for i = 1:length(metalst)
+		set_series(oxr, i-1)
+		img = open_img(oxr, 0)
+		properties = Dict{String, Any}()
+		for a = attributes(metalst[i])
+			push!(properties, "Image$(name(a))"=>value(a))
+		end
+		for a = attributes(find_element(metalst[i], "Pixels"))
+			push!(properties, "Pixel$(name(a))"=>value(a))
+		end
+
+		push!(images, ImageMeta(img, properties))
+	end
+	return images
+end
+
+end
