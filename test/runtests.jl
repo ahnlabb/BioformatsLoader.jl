5f8141aab005f9b5f5c773f052a2f467b7248997
Johannes Ahnlide <johannes@voxel.se>
2018-02-09 13:59:10 +0100

    Basic test and removed create_service from export

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
 using BioformatsLoader
 using Base.Test
+using JavaCall
 
-# write your own tests here
-@test 1 == 2
+pkgname = "BioformatsLoader"
+bfpkg_path = joinpath(Pkg.dir(), pkgname, "bioformats_package.jar")
+
+download("https://downloads.openmicroscopy.org/bio-formats/5.7.3/artifacts/bioformats_package.jar", bfpkg_path)
+
+JavaCall.init(["-verbose:gc","-Djava.class.path=$bfpkg_path"])
+
+oxr = OMEXMLReader()
+@test oxr.reader.ptr != C_NULL
+@test oxr.meta.ptr != C_NULL
