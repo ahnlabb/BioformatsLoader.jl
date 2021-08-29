ef7f038e7109a443ab157c1d06402a17e7dbad79
Tim Holy <tim.holy@gmail.com>
2021-08-29 05:50:30 -0500

    Don't assume SizeC Channel attributes

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,20 @@ oxr = OMEXMLReader()
 @test !JavaCall.isnull(oxr.reader)
 @test !JavaCall.isnull(oxr.meta)
 
+openjpeg_imgs_url = "https://github.com/uclouvain/openjpeg-data/raw/master/input/conformance"
+
+mktempdir() do path
+    for file in ("file2.jp2",)
+        imgpath = joinpath(path, file)
+        Downloads.download(joinpath(openjpeg_imgs_url, file), imgpath)
+        md = metadata(imgpath)
+        @test md["SizeC"] == 3
+        @test md["Type"] == "uint8"
+        img = bf_import(imgpath)
+        @test size(img[1])[1:3] == (3, md["SizeY"], md["SizeX"])
+    end
+end
+
 ome_imgs_url = "https://downloads.openmicroscopy.org/images"
 
 ome_paths =
